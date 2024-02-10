21ee4218571dca3ab8254e349af3a253f83b93fd
Fethi Hamdani <fethi.io.dz@gmail.com>
2024-02-10 17:37:18 +0000

    added basic hud elements

---
+++ lib/hud/pause/pause_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class PauseScreen extends StatelessWidget {
+  const PauseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/hud/widgets/card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
